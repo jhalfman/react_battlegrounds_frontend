48dc41aaf40317a4302a7e81743d0e04d161af1e
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-12 14:25:53 -0700

    add tribe message fetch request and displaying on home page"

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import './App.css';
 import {useEffect, useState} from 'react';
 
 function App() {
-  const [routeMessage, setRouteMessage] = useState("");
+  const [homeMessage, setHomeMessage] = useState("");
+  const [cardMessage, setCardMessage] = useState("");
+  const [tribeMessage, setTribeMessage] = useState([]);
+  const [tierMessage, setTierMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:9292")
     .then(resp => resp.json())
-    .then(data => setRouteMessage(data.message))
-  })
+    .then(data => setHomeMessage(data.message))
+    
+    fetch("http://localhost:9292/tribes")
+    .then(resp => resp.json())
+    .then(data => setTribeMessage(data))
+    
+    /* fetch("http://localhost:9292")
+    .then(resp => resp.json())
+    .then(data => setHomeMessage(data.message)) */
+
+    /* fetch("http://localhost:9292")
+    .then(resp => resp.json())
+    .then(data => setHomeMessage(data.message)) */
+  }, [])
 
   return (
     <div className="App">
-      {routeMessage}
+      {homeMessage}
+      {tribeMessage.map(tribe => {
+        return <div>{tribe.name}</div>
+      })}
     </div>
   );
 }
