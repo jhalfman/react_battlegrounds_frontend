ebc4021ffdc68d15d4b25026e7fbc5f0d6ec2d58
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-14 14:31:36 -0700

    add Builds, Cards, Home components. Add build, cards, home routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useEffect, useState} from 'react';
 import NavBar from './components/navigation/NavBar';
 import {Routes, Route } from 'react-router-dom';
+import Cards from './components/cards/Cards';
+import Home from './components/static/Home';
+import Builds from './components/cards/Builds';
 
 function App() {
   const [homeMessage, setHomeMessage] = useState("");
@@ -14,8 +17,9 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<div>HEllo</div>} />
-        <Route path="/cards" element={<div>cards</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/builds" element={<Builds />} />
       </Routes>
     </div>
   );
