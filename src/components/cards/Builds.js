1ea9d5853287862f650c94371b65c62c4fbbc3ff
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-17 08:46:13 -0700

    adding builds fetch request and displaying build names

--- src/components/cards/Builds.js
+++ src/components/cards/Builds.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Builds = () => {
+  const [buildList, setBuildList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/builds")
+    .then(resp => resp.json())
+    .then(data => setBuildList(data))
+  }, [])
+
+
   return (
-    <div>Builds</div>
+    <div>
+      {buildList.map(build => {
+        return build.name
+      })}
+    </div>
   )
 }
 
