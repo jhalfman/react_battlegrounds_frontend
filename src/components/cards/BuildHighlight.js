d4299470fcf643c1750e58ea7552d36a9ce040c5
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-17 09:57:19 -0700

    create build enabled

--- src/components/cards/BuildHighlight.js
+++ src/components/cards/BuildHighlight.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from './Card';
+import { useNavigate } from "react-router-dom";
 
 const BuildHighlight = () => {
+    const [currentBuild, setCurrentBuild] = useState([])
     let {id} = useParams();
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/builds/${id}`)
+        .then(resp => resp.json())
+        .then(data => setCurrentBuild(data))
+    }, [])
+
+    function backToBuilds() {
+        navigate("/builds")
+    }
+
+    
   return (
-    <div>{id}</div>
+    <div>
+        {currentBuild.map(card => {
+            return <Card key={card.id} card={card} />
+        })}
+        <button>Edit Build</button>
+        <button>Delete Build</button>
+        <button onClick={backToBuilds}>Back</button>
+    </div>
   )
 }
 
--- src/components/cards/Builds.js
+++ src/components/cards/Builds.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Build from './Build';
+import { useNavigate } from "react-router-dom";
 
 const Builds = () => {
   const [buildList, setBuildList] = useState([]);
+  let navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:9292/builds")
@@ -10,7 +12,28 @@ const Builds = () => {
     .then(data => setBuildList(data))
   }, [])
 
- 
+ function createBuild() {
+  console.log("Creating new build")
+  fetch("http://localhost:9292/builds", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: "post tester 1",
+      card1: 8,
+      card2: 37,
+      card3: 89,
+      card4: 14,
+      card5: 109,
+      card6: 110,
+      card7: 111
+    }),
+  })
+  .then(resp => resp.json())
+  .then(data => navigate(`${data.id}`))
+  
+ }
 
 
   return (
@@ -18,7 +41,7 @@ const Builds = () => {
       {buildList.map(build => {
         return <Build key={build.name} name={build.name} id={build.id}/>
       })}
-      <button id='add_build'>Add Build</button>
+      <button id='add_build' onClick={createBuild}>Add Build</button>
     </div>
   )
 }
